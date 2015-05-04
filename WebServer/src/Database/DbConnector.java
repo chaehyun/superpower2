bf4a01943025907c5922516b194f95872264c5c3
Seongjun Park <sjpark0233@gmail.com>
2015-05-04 09:13:31 +0900

    DbConnector - 싱글톤

--- WebServer/src/Database/DbConnector.java
+++ WebServer/src/Database/DbConnector.java
@@ -4,22 +4,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbConnector {
+	
+	// for singletone
+	private static DbConnector instance = null;
 
 	private String url = "jdbc:mysql://localhost:3306/super_power";
 	private String user = "root";
 	private String password = "root";
 	private Connection connection;
 
+	// 싱글톤 객체 반환
+	public static DbConnector getInstance() {
+		if(instance == null)
+			instance = new DbConnector();
+		
+		return instance;
+	}
+	
 	// 생성자 - DB와 연결함.
-	public DbConnector() {
+	private DbConnector() {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(url, user, password);
+			this.connection = DriverManager.getConnection(url, user, password);
 
 			System.out.println("Success - Connect MySQL");
 		} catch (Exception e) {
 			System.out.println("Failed - Connect MySQL");
 		}
+	}
+	
+	// connection 반환
+	public Connection getConnection() {
+		return this.connection;
 	}
 }
