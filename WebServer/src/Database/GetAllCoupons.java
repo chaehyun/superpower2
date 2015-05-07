55afb5eb3746bfbffc739b40f929b1d01a964857
Seongjun Park <sjpark0233@gmail.com>
2015-05-07 14:59:56 +0900

    merged

---
+++ WebServer/src/Database/GetAllCoupons.java
@@ -0,0 +1,37 @@
+package Database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Elements.Coupon;
+import Elements.Member;
+
+public class GetAllCoupons {
+	synchronized public static List<Coupon> doAction() throws SQLException {
+
+		// 쿼리 실행
+		String sql = "select * from coupon";
+		PreparedStatement statement = DbConnector.getInstance().getConnection()
+				.prepareStatement(sql);
+		ResultSet resultSet = statement.executeQuery();
+
+		// 결과들을 ArrayList에 저장
+		List<Coupon> couponList = new ArrayList<Coupon>();
+		
+		while (resultSet.next()) {
+			Coupon coupon = new Coupon();
+			coupon.setc_code(resultSet.getString("c_code"));
+			coupon.seti_code(resultSet.getString("i_code"));
+			coupon.setdiscount(resultSet.getInt("discount"));
+			coupon.setbegin_date(resultSet.getDate("begin_date"));
+			coupon.setend_date(resultSet.getDate("end_ate"));
+
+			couponList.add(coupon);
+		}
+
+		return couponList;
+	}
+}
