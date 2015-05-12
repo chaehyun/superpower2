51f71cfded710728269f3c43b1b2b468307e06bb
Seongjun Park <sjpark0233@gmail.com>
2015-05-12 20:36:49 +0900

    implemented ShowingCoupon

--- WebServer/src/Communication/ClientThread.java
+++ WebServer/src/Communication/ClientThread.java
@@ -3,10 +3,10 @@ package Communication;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +15,7 @@ import org.json.JSONObject;
 
 import Service.BestItem;
 import Service.Login;
+import Service.ShowCoupon;
 
 /**
  * 클라이언트에게 서비스를 제공하는 스레드 클래스.
@@ -74,7 +75,7 @@ public class ClientThread extends Thread {
 					break;
 				}
 				JSONObject recvMsg = new JSONObject(line);
-				
+
 				switch (recvMsg.getString("MessageType")) {
 				case "req_login":
 					sendlogin(recvMsg);
@@ -87,7 +88,7 @@ public class ClientThread extends Thread {
 					break;
 				}
 			}
-		} catch (IOException | JSONException e) {
+		} catch (Exception e) {
 		}
 
 		// 클라이언트 종료 및 서버에서 리스트로부터 제거됨.
@@ -129,11 +130,11 @@ public class ClientThread extends Thread {
 					+ e.getMessage());
 		}
 	}
-	
-	public void sendlogin(JSONObject recvMsg) throws JSONException{
-		
+
+	public void sendlogin(JSONObject recvMsg) throws JSONException {
+
 		JSONObject sendMsg = new JSONObject();
-		
+
 		sendMsg = Login.logincheck(recvMsg.getString("ID"),
 				recvMsg.getString("Password"));
 		if (sendMsg.getBoolean("Result")) {
@@ -144,20 +145,45 @@ public class ClientThread extends Thread {
 		this.printWriter.println(sendMsg.toString());
 		this.printWriter.flush();
 	}
-	
-	public void sendbestitem() throws JSONException{
-		
+
+	public void sendbestitem() throws JSONException {
+
 		List<JSONObject> bestlist = new ArrayList<JSONObject>();
 		bestlist = BestItem.getBestItems();
-		
-		for(int i=0; i<10; i++){
+
+		for (int i = 0; i < 10; i++) {
 			JSONObject sendMsg = new JSONObject();
 			sendMsg = bestlist.get(i);
-			
+
 			System.out.println("send-> " + sendMsg.getString("Minor"));
 			this.printWriter.println(sendMsg.toString());
 			this.printWriter.flush();
 		}
-		
+	}
+
+	/**
+	 * 해당 회원의 쿠폰리스트를 보냄
+	 * 
+	 * @param recvMsg
+	 * @throws JSONException
+	 * @throws SQLException
+	 */
+	public void sendCouponList(JSONObject recvMsg) throws Exception {
+
+		// 해당 회원의 쿠폰 수를 구함
+		this.printWriter.println(ShowCoupon.getCouponCount(this.id));
+		this.printWriter.flush();
+ 
+		// ACK를 받음. 수신실패하거나 헤더가 다를경우 예외발생
+		String ack = this.bufferedReader.readLine();
+		if (ack == null
+				|| !(new JSONObject(ack).getString("MessageType")
+						.equals("req_coupon_list_ack"))) {
+			throw new Exception();
+		}
+
+		// 해당 회원이 소유하는 쿠폰 데이터 덩어리를 소켓으로 보냄
+		this.printWriter.println(ShowCoupon.getCoupons(this.id));
+		this.printWriter.flush();
 	}
 }
---
+++ WebServer/src/Service/ShowCoupon.java
@@ -0,0 +1,95 @@
+package Service;
+
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import Database.GetAllOwnership;
+import Database.GetCoupon;
+import Database.GetItem;
+import Elements.Coupon;
+import Elements.Item;
+import Elements.Ownership;
+
+/**
+ * 특정 고객의 쿠폰 목록을 조회. 회원ID를 파라미터로 함
+ * 
+ * @author Seongjun, Minji
+ * @since 2015/5/12
+ * @version 2015/5/12
+ */
+public class ShowCoupon {
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 * @throws JSONException
+	 */
+	synchronized public static JSONObject getCouponCount(String id) throws SQLException, JSONException {
+
+		// 해당 회원ID의 소유 쿠폰 수 카운트
+		int count = 0;
+		for (Ownership ownership : GetAllOwnership.doAction()) {
+			if (id.equals(ownership.getId())) {
+				count++;
+			}
+		}
+
+		// JSON화
+		JSONObject resultObject = new JSONObject();
+		resultObject.put("MessageType", "res_coupon_list_count");
+		resultObject.put("Count", count);
+		
+		return resultObject;
+	}
+	
+	/**
+	 * @param id
+	 *            회원ID
+	 * @return 쿠폰데이터
+	 */
+	synchronized public static JSONObject getCoupons(String id)
+			throws JSONException, SQLException {
+
+		// 헤더 추가
+		JSONObject resultObject = new JSONObject();
+		resultObject.put("MessageType", "res_coupon_list_datas");
+
+		// 데이터 부분
+		JSONArray jsonArray = new JSONArray();
+		for (Ownership ownership : GetAllOwnership.doAction()) {
+			
+			// 소유테이블의 레코드 중 해당 회원의 ID와 비교
+			if (id.equals(ownership.getId())) {
+				
+				// 소유 레코드의 쿠폰코드와 상품코드를 이용하여 객체 찾음
+				Coupon coupon = GetCoupon.doAction(ownership.getC_code());
+				Item item = GetItem.doAction(coupon.geti_code());
+				
+				// jsonArray에 넣을 쿠폰데이터
+				JSONObject jsonCoupon = new JSONObject();
+				jsonCoupon.put("Code", coupon.getc_code());
+				jsonCoupon.put("ItemName", item.getminor());
+				jsonCoupon.put("Price", item.getprice());
+				jsonCoupon.put("Discount", coupon.getdiscount());
+				jsonCoupon.put("StartDate", new SimpleDateFormat("yyyy-MM-dd")
+						.format(coupon.getbegin_date()));
+				jsonCoupon.put("EndDate", new SimpleDateFormat("yyyy-MM-dd")
+						.format(coupon.getbegin_date()));
+
+				// jsonArray에 넣음
+				jsonArray.put(jsonCoupon);
+			}
+		}
+		
+		// 쿠폰 관련 데이터 넣음
+		resultObject.put("Coupons",jsonArray);
+
+		return resultObject;
+	}
+}
